dc3207193f9ba75d23238478c2c53a64196743a3
Lukasz Wasielewski <lukasz482@wp.pl>
2019-01-24 21:33:34 +0100

    Added conceptual model

---
+++ HospitalClassLibrary/model/uzytkownicy/pracownicy/Pielegniarka.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalClassLibrary.model.uzytkownicy.pracownicy
+{
+    class Pielegniarka
+    {
+    }
+}
